26c5b62138306fa15b3d050dec6a6082ecd391ea
Łukasz Kalita <lukkalita@onet.pl>
2024-04-18 16:13:54 +0200

    Plane Service

--- SPAirlines/src/main/java/com/lucaskalita/airlines/plane/PlaneService.java
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/plane/PlaneService.java
@@ -18,39 +18,69 @@ public class PlaneService {
     @Autowired
     PlaneMapper planeMapper;
 
-    public Plane findPlanetById(Long id) {
+    public PlaneDTO findPlanetById(Long id) {
         log.info("Searching for Plane by ID: {}", id);
         return planeRepository
                 .findById(id)
                 .map(plane -> {
                     log.info("Aircraft found: {}", plane);
-                    return plane;
+                    return planeMapper.fromEntityToDto(plane);
                 })
                 .orElseThrow(() -> new WrongFlightIDException("No plane with this id: " + id));
     }
-    public List<Plane> findAllPlanes(){
+    public List<PlaneDTO> findAllPlanes(){
         log.info("Searching for all planes");
-        return planeRepository.findAll();
+        return planeRepository.findAll()
+                .stream()
+                .map(planeMapper::fromEntityToDto)
+                .collect(Collectors.toList());
     }
-    public List<Plane> findPlanesByBrand (PlaneBrand brand){
+    public List<PlaneDTO> findPlanesByBrand (PlaneBrand brand){
         log.info("Search all {} Planes", brand);
         return planeRepository.findAll()
                 .stream()
                 .filter(n -> n.getPlaneBrand().equals(brand))
+                .map(planeMapper::fromEntityToDto)
                 .collect(Collectors.toList());
     }
-    public List<Plane> findPlanesByBrand (PlaneModel model){
+    public List<PlaneDTO> findPlanesByBrand (PlaneModel model){
         log.info("Search all {} Aircraft", model);
         return planeRepository.findAll()
                 .stream()
                 .filter(n -> n.getPlaneModel().equals(model))
+                .map(planeMapper::fromEntityToDto)
                 .collect(Collectors.toList());
     }
-    public List<Plane> findAllPlanesWithMoreTotalSeatsThan(int x){
+    public List<PlaneDTO> findAllPlanesWithMinimalNumberOfSeats(int x){
         log.info("Searching all planes with more seats than {}", x);
         return planeRepository.findAll()
                 .stream()
-                .filter(n -> n.getTotalNumberOfSeats() > x)
+                .filter(n -> n.getListOfPremiumSeats().size() + n.getListOfRegularSeats().size() >= x)
+                .map(planeMapper::fromEntityToDto)
+                .collect(Collectors.toList());
+    }
+    public List<PlaneDTO> findAllPlanesWithMaximalNumberOfSeats(int x){
+        log.info("Searching all planes with maximal seats amount: {}", x);
+        return planeRepository.findAll()
+                .stream()
+                .filter(n -> n.getListOfPremiumSeats().size() + n.getListOfRegularSeats().size() <= x)
+                .map(planeMapper::fromEntityToDto)
+                .collect(Collectors.toList());
+    }
+    public List<PlaneDTO> findAllPlanesWithMinimalNumberOfRegularSeats(int x){
+        log.info("Searching all planes with minimal regular seats amount: {}", x);
+        return planeRepository.findAll()
+                .stream()
+                .filter(n -> n.getListOfRegularSeats().size() >= x)
+                .map(planeMapper::fromEntityToDto)
+                .collect(Collectors.toList());
+    }
+    public List<PlaneDTO> findAllPlanesWithMinimalNumberOfPremiumSeats(int x){
+        log.info("Searching all planes with minimal premium seats amount: {}", x);
+        return planeRepository.findAll()
+                .stream()
+                .filter(n -> n.getListOfPremiumSeats().size() >= x)
+                .map(planeMapper::fromEntityToDto)
                 .collect(Collectors.toList());
     }
 
