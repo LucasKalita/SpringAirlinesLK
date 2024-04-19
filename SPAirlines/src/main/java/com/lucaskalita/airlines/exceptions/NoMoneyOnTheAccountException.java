5ad6866ef6236e3496e7f849326e9c4c76d92c31
Łukasz Kalita <lukkalita@onet.pl>
2024-04-19 17:34:38 +0200

    created nomoney expection and wrong date exception

---
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/exceptions/NoMoneyOnTheAccountException.java
@@ -0,0 +1,6 @@
+package com.lucaskalita.airlines.exceptions;
+
+public class NoMoneyOnTheAccountException extends Throwable {
+    public NoMoneyOnTheAccountException(String s) {
+    }
+}
---
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/exceptions/WrongDateException.java
@@ -0,0 +1,6 @@
+package com.lucaskalita.airlines.exceptions;
+
+public class WrongDateException extends RuntimeException {
+    public WrongDateException(String s) {
+    }
+}
