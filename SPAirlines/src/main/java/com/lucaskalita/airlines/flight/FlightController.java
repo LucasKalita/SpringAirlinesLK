8bade68e04fa21d9d90378c90cfc67e92f669930
Łukasz Kalita <lukkalita@onet.pl>
2024-05-28 17:17:27 +0200

    Added controllers for flightService

--- SPAirlines/src/main/java/com/lucaskalita/airlines/flight/FlightController.java
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/flight/FlightController.java
@@ -1,9 +1,13 @@
 package com.lucaskalita.airlines.flight;
 
+import com.lucaskalita.airlines.airport.Airport;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/flights")
@@ -21,6 +25,7 @@ public class FlightController {
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.FOUND)
     public FlightDTO getFlightById(@PathVariable Long id){
+
         return flightService.findFlightById(id);
     }
     @DeleteMapping("/{id}")
@@ -38,12 +43,56 @@ public class FlightController {
     public FlightDTO findByPlaneId(@PathVariable Long planeId){
         return flightService.findFlightByPlaneId(planeId);
     }
-    @GetMapping("/empty/{id}")
+    @GetMapping("/seats/{id}")
     @ResponseStatus(HttpStatus.OK)
     public String checkForEmptySeats(@PathVariable Long id){
         return flightService.checkForEmptySeats(id);
     }
-
+    @GetMapping("/timeBeforeArrival/{arrTime}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsBeforeArrivalDate(@PathVariable LocalDateTime arrTime){
+        return flightService.findFlightsBeforeArrivalDate(arrTime);
+    }
+    @GetMapping("/timeAfterDeparture/{arrTime}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsAfterArrivalDate(@PathVariable LocalDateTime arrTime){
+        return flightService.findFlightsAfterArrivalDate(arrTime);
+    }
+    @GetMapping("/timeBeforeArrival/{depTime}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsBeforeDepartureDate(@PathVariable LocalDateTime depTime) {
+        return flightService.findFlightsByDepartureDateBefore(depTime);
+    }
+    @GetMapping("/timeAfterDeparture/{depTime)")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsAfterDepartureDate(@PathVariable LocalDateTime depTime){
+       return flightService.findFlightsByDepartureDateAfter(depTime);
+    }
+    @GetMapping("/timeBetweenArrival/{arrTime1}-{arrTime2}")
+    @ResponseStatus(HttpStatus.FOUND)
+            public List<FlightDTO> findFlightsBetweenArrival(@PathVariable LocalDateTime arrTime1, @PathVariable LocalDateTime arrTime2) {
+        return flightService.findFlightsBetweenArrivalDates(arrTime1, arrTime2);
+    }
+    @GetMapping("/timeBetweenArrival/{depTime1}-{depTime2}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsBetweenDeparture(@PathVariable LocalDateTime depTime1, @PathVariable LocalDateTime depTime2) {
+        return flightService.findFlightsByDepartureDateBetween(depTime1, depTime2);
+    }
+    @GetMapping("/arrivalAirport/{airport}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsToAirport(@PathVariable Airport airport){
+        return flightService.findFlightsByArrivalAirport(airport);
+    }
+    @GetMapping("/departureAirport/{airport}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsFromAirport(@PathVariable Airport airport){
+        return flightService.findFlightsByDepartureAirport(airport);
+    }
+    @GetMapping("/betweenAirports/{airport1}-{airport2}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<FlightDTO> findFlightsFromAirport1ToAirport2(@PathVariable Airport airport1, @PathVariable Airport airport2){
+        return flightService.findFlightsBetweenAirports(airport1,airport2);
+    }
 }
 
 
