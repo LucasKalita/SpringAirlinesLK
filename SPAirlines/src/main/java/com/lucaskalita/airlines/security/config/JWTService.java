cc453975a027c29d2e7b3b2e18bc9759ebdb9fc8
Łukasz Kalita <lukkalita@onet.pl>
2024-07-02 16:41:26 +0200

    generate token for user, both options

--- SPAirlines/src/main/java/com/lucaskalita/airlines/security/config/JWTService.java
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/security/config/JWTService.java
@@ -2,22 +2,43 @@ package com.lucaskalita.airlines.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JWTService {
     private static final String secretKey = "d8b26c3d6209e6c450c07ee5534043dd6d729d9c1a9d44645d063d0a5d3a40c6";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+    public String generateToken (UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(
+        Map<String, Object> extraClaims,
+        UserDetails userDetails
+        ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
     private Claims extractAllClaims(String token){
         return Jwts
