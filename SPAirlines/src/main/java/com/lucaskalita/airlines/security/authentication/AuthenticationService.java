62d93fd94e91d13e74b7c5afd5e0f76f174155c8
Łukasz Kalita <lukkalita@onet.pl>
2024-07-04 19:51:18 +0200

    authentication and register methods

--- SPAirlines/src/main/java/com/lucaskalita/airlines/security/authentication/AuthenticationService.java
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/security/authentication/AuthenticationService.java
@@ -1,10 +1,13 @@
 package com.lucaskalita.airlines.security.authentication;
 
+import com.lucaskalita.airlines.globalExceptions.ObjectNotFoundException;
 import com.lucaskalita.airlines.security.config.JWTService;
 import com.lucaskalita.airlines.users.AccountType;
 import com.lucaskalita.airlines.users.User;
 import com.lucaskalita.airlines.users.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
+
     public AuthenticationResponse register(RegisterRequest registerRequest) {
         var user = User.builder()
                 .name(registerRequest.getName())
@@ -28,7 +33,9 @@ public class AuthenticationService {
                 .password(passwordEncoder.encode(registerRequest.getPassword()))
                 .accountType(AccountType.REGISTERED)
                 .build();
+
         userRepository.save(user);
+
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
@@ -36,6 +43,20 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticateRequest authenticateRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticateRequest.getUsername(),
+                        authenticateRequest.getPassword()
+                )
+        );
 
+        var user = userRepository.findByUsername(authenticateRequest.getUsername())
+                .orElseThrow(
+                        () -> new ObjectNotFoundException("No object by this parapeter: " +
+                                authenticateRequest.getUsername()));
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
