6828cec1acb1cc011af67d6aebcb9a1d647911ef
Łukasz Kalita <lukkalita@onet.pl>
2024-07-04 18:13:23 +0200

    Rest controler for auth (commit for visual bug)

---
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/security/authentication/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.lucaskalita.airlines.security.authentication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AuthenticationController {
+}
