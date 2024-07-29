c77be3ceb27c7f10cee4d876deabf02f78c0c1ae
Łukasz Kalita <lukkalita@onet.pl>
2024-07-29 18:33:12 +0200

    newst veriosn of sec conf according to Sping sec 6 standard

--- SPAirlines/src/main/java/com/lucaskalita/airlines/security/config/SecurityConfiguration.java
+++ SPAirlines/src/main/java/com/lucaskalita/airlines/security/config/SecurityConfiguration.java
@@ -1,15 +1,13 @@
 package com.lucaskalita.airlines.security.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,25 +17,34 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/v1/auth/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFiler, UsernamePasswordAuthenticationFilter.class);
-                
+//        http
+//                .csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/v1/auth/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFiler, UsernamePasswordAuthenticationFilter.class);
 
+        return http.authorizeHttpRequests(authorizeHttp -> {
+                            authorizeHttp.requestMatchers("/").permitAll();
+                            authorizeHttp.requestMatchers("/flights").permitAll();
+                            authorizeHttp.requestMatchers("/airports").permitAll();
+                            authorizeHttp.requestMatchers("/users").hasRole("ADMIN");
+                            authorizeHttp.anyRequest().authenticated();
+                        }
+                ).formLogin(l -> l.defaultSuccessUrl("/loggedIn"))
+                .logout(l -> l.logoutSuccessUrl("/loggedOut"))
+                .oauth2Login(Customizer.withDefaults())
+                .build();
 
-        return http.build();
     }
 }
